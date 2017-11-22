5b865b4238f22f98b7f65f2cfe1cf9c92d27c302
Eduardo Evaldt <eduardowevaldt@hotmail.com>
2017-11-22 19:53:46 -0200

    Algoritmo para alteraçoes de salario

---
+++ Algoritmo Salario/revisao 6.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+
+float salario,i,num;
+
+printf("Informe o numero de funcionarios: ");
+scanf("%f",&num);
+
+
+for(i=0;i<num;i++){
+
+printf("\nInforme o salario do funcionario: ");
+scanf("%f",&salario);
+
+if(salario<=500){
+salario=salario*1.05;
+}
+
+else if(salario<1200){
+salario=salario+1.12;
+}
+
+if(salario<=600){
+salario=salario+150;
+}
+
+else{
+salario=salario+100;
+}
+
+printf("\nO novo salario e: %f",salario);
+}
+
+getche();
+}
